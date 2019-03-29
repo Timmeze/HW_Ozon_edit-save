9315952a69a41cb29ecc605982a2fe82a0faf282
MezentsevTA <timur-mezentsev@yandex.ru>
2019-03-29 11:43:12 +0300

    prepearings for heroku #6

--- app/main.py
+++ app/main.py
@@ -74,7 +74,7 @@ def start():
 
 
     if os.getenv('APP_ENV') == 'PROD' and os.getenv('PORT'):
-        waitress.serv(app, port=os.getenv('PORT'))
+        waitress.serve(app, port=os.getenv('PORT'))
     else:
         app.run(port=9843, debug=True)
 
